bd164b7e446f14132717e4cae9c986a88b2a60c3
Oscar <oscar.ims93@gmail.com>
2019-11-25 21:25:16 -0600

    add cards

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Camera from '@material-ui/icons/Camera'  
 import NavBar from './Navbar'
-import { purple, blue, green } from '@material-ui/core/colors'
+import { purple, green } from '@material-ui/core/colors'
+import Mediacard from './cards'
 
 const theme = createMuiTheme({
   palette: {
@@ -24,6 +25,7 @@ class App extends Component {
               Welcome to Material UI
           </Button>
           <TextField placeholder="Enter your Name" label="Basic TextField"></TextField>
+          <Mediacard/>
       </MuiThemeProvider>
     )
   }
--- src/cards.js
+++ src/cards.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -8,9 +8,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import IMG from './bills.jpg';
-
-
+import IMG from './bills.jpg'; const styles = { card: { maxWidth: 345, }, media: { height: 140, }, };
 function MediaCard(props) { const { classes } = props;
   return (
     <Card className={classes.card}>
@@ -28,7 +26,5 @@ function MediaCard(props) { const { classes } = props;
     </Card>
   );
 }
-
-
 MediaCard.propTypes = { classes: PropTypes.object.isRequired, };
-export default MediaCard;
+export default withStyles(styles)(MediaCard);
